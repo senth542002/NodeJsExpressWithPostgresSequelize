a93c46cc7154b944fc406c1a4f59ee7058632fad
senth542002 <senth542002@gmail.com>
2019-05-09 19:10:25 +0530

    Commiting formatted Code

--- app/controllers/todoitems.js
+++ app/controllers/todoitems.js
@@ -1,57 +1,58 @@
-const TodoItem = require('../../models').TodoItem;
+const TodoItem = require('../../models').TodoItem
 
 module.exports = {
-    create(req, res){
-      return TodoItem
-            .create({
-              content: req.body.content,
-              todoId: req.params.todoId,
-            })
-            .then(todoItem => res.status(201).send(todoItem))
-            .catch(error => res.status(400).send(error));
-    },
+  create (req, res) {
+    return TodoItem.create({
+      content: req.body.content,
+      todoId: req.params.todoId
+    })
+      .then(todoItem => res.status(201).send(todoItem))
+      .catch(error => res.status(400).send(error))
+  },
 
-    update(req, res){
-      return TodoItem
-                    .findOne({
-                      where: {
-                        id: req.params.todoItemId,
-                        todoId: req.params.todoId,
-                      },
-                    })
-                    .then(todoItem => {
-                      if(!todoItem) {
-                        return res.status(404).send({message: 'Todo Item not found',
-                        });
-                      }
-                      return todoItem.update({
-                        content: req.body.content || todoItem.content,
-                        complete: req.body.complete || todoItem.complete,
-                      })
-                      .then(updatedTodoItem => res.status(200).send(updatedTodoItem))
-                      .catch(error => res.status(400).send(error+'Unable Update Todo Item1'));
-                    })
-                    .catch(error => res.status(400).send(error+'Unable Update Todo Item2'));
-    },
+  update (req, res) {
+    return TodoItem.findOne({
+      where: {
+        id: req.params.todoItemId,
+        todoId: req.params.todoId
+      }
+    })
+      .then(todoItem => {
+        if (!todoItem) {
+          return res.status(404).send({ message: 'Todo Item not found' })
+        }
+        return todoItem
+          .update({
+            content: req.body.content || todoItem.content,
+            complete: req.body.complete || todoItem.complete
+          })
+          .then(updatedTodoItem => res.status(200).send(updatedTodoItem))
+          .catch(error =>
+            res.status(400).send(error + 'Unable Update Todo Item1')
+          )
+      })
+      .catch(error => res.status(400).send(error + 'Unable Update Todo Item2'))
+  },
 
-    destroy(req, res) {
-      return TodoItem
-                    .findOne({
-                      where: {
-                        id: req.params.todoItemId,
-                        todoId: req.params.todoId,
-                      },
-                    })
-                    .then(todoItem => {
-                      console.log(todoItem);
-                      if(!todoItem) {
-                        return res.status(404).send({message: 'Todo Item not found',
-                        });
-                      }
-                      return todoItem.destroy()
-                                      .then(() => res.status(200).send({message: 'Todo Item deleted successfully.' }))
-                                      .catch(error => res.status(400).send(error+'Todo1'));
-                      })
-                      .catch(error => res.status(400).send(error+'Todo2'));
-    },
-};
+  destroy (req, res) {
+    return TodoItem.findOne({
+      where: {
+        id: req.params.todoItemId,
+        todoId: req.params.todoId
+      }
+    })
+      .then(todoItem => {
+        console.log(todoItem)
+        if (!todoItem) {
+          return res.status(404).send({ message: 'Todo Item not found' })
+        }
+        return todoItem
+          .destroy()
+          .then(() =>
+            res.status(200).send({ message: 'Todo Item deleted successfully.' })
+          )
+          .catch(error => res.status(400).send(error + 'Todo1'))
+      })
+      .catch(error => res.status(400).send(error + 'Todo2'))
+  }
+}
--- app/controllers/todos.js
+++ app/controllers/todos.js
@@ -1,66 +1,73 @@
-const Todo = require('../../models').Todo;
-const TodoItem = require('../../models').TodoItem;
+const Todo = require('../../models').Todo
+const TodoItem = require('../../models').TodoItem
 
 module.exports = {
-  create(req, res) {
-    return Todo.create({title: req.body.title})
-               .then(todo => res.status(201).send(todo))
-               .catch(error => res.status(400).send(error));
+  create (req, res) {
+    return Todo.create({ title: req.body.title })
+      .then(todo => res.status(201).send(todo))
+      .catch(error => res.status(400).send(error))
   },
 
-  list(req, res) {
+  list (req, res) {
     return Todo.findAll({
-                  include: [{
-                    model: TodoItem,
-                    as: 'todoItems',
-                  }],
-                })
-               .then(todos => res.status(200).send(todos))
-               .catch(error => res.status(400).send(error));
+      include: [
+        {
+          model: TodoItem,
+          as: 'todoItems'
+        }
+      ]
+    })
+      .then(todos => res.status(200).send(todos))
+      .catch(error => res.status(400).send(error))
   },
 
-  update(req, res) {
+  update (req, res) {
     return Todo.findByPk(req.params.todoId, {
-                  include: [{
-                    model: TodoItem,
-                    as: 'todoItems',
-                  }],
-                })
-                .then(todo => {
-                  if(!todo){
-                    return res.status(404).send({
-                      message: 'Todo not found',
-                    });
-                  }
-                  return todo
-                    .update({
-                      title: req.body.title || todo.title,
-                    })
-                    .then(todo => res.status(200).send(todo))
-                    .catch(error => res.status(400).send(error));
-                })
-                .catch(error => res.status(400).send(error));
+      include: [
+        {
+          model: TodoItem,
+          as: 'todoItems'
+        }
+      ]
+    })
+      .then(todo => {
+        if (!todo) {
+          return res.status(404).send({
+            message: 'Todo not found'
+          })
+        }
+        return todo
+          .update({
+            title: req.body.title || todo.title
+          })
+          .then(todo => res.status(200).send(todo))
+          .catch(error => res.status(400).send(error))
+      })
+      .catch(error => res.status(400).send(error))
   },
 
-  destroy(req, res) {
-    return Todo
-              .findByPk(req.params.todoId, {
-                            include: [{
-                              model: TodoItem,
-                              as: 'todoItems',
-                            }],
-                          })
-                          .then(todo => {
-                            if(!todo){
-                              return res.status(404).send({
-                                message: 'Todo not found',
-                              });
-                            }
-                            return todo
-                              .destroy()
-                              .then(() => res.status(200).send({ message: 'Todo deleted successfully.' }))
-                              .catch(error => res.status(400).send(error));
-                          })
-                          .catch(error => res.status(400).send(error));
-  },
-};
+  destroy (req, res) {
+    return Todo.findByPk(req.params.todoId, {
+      include: [
+        {
+          model: TodoItem,
+          as: 'todoItems'
+        }
+      ]
+    })
+      .then(todo => {
+        if (!todo) {
+          return res.status(404).send({
+            message: 'Todo not found'
+          })
+        }
+        return todo
+          .destroy()
+          .then(() =>
+            res.status(200).send({ message: 'Todo deleted successfully.' })
+          )
+          .catch(error => res.status(400).send(error))
+      })
+      .catch(error => res.status(400).send(error))
+  }
+}
