ec2fdd8e459aad6586daf75abc21f2562a3076d6
senth542002 <senth542002@gmail.com>
2019-05-18 06:13:14 +0530

    Added Console Log

--- app/controllers/todos.js
+++ app/controllers/todos.js
@@ -5,7 +5,10 @@ module.exports = {
   create (req, res) {
     return Todo.create({ title: req.body.title })
       .then(todo => res.status(201).send(todo))
-      .catch(error => res.status(400).send(error))
+      .catch(error => {
+        console.log(error);
+        res.status(400).send(error)
+      })
   },
 
   list (req, res) {
